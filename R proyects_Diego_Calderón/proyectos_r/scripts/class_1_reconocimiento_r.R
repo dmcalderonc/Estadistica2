de5078bc0f3d92ae902b2ac54fdd945006a30915
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-17 12:06:57 -0500

    cambios

--- R proyects_Diego_Calderón/proyectos_r/scripts/class_1_reconocimiento_r.R
+++ R proyects_Diego_Calderón/proyectos_r/scripts/class_1_reconocimiento_r.R
@@ -5,6 +5,7 @@
 #FECHA:14/102025
 #TEMA: INDRODUCCION AL SOFTWARE R
 #######################################################################
+# Tema 1
 # Sample vector
 values <- c(2, 5, 6, 1)
 
@@ -13,20 +14,23 @@ res <- ifelse(values > 4, TRUE, FALSE)
 
 # Output
 res
-#Tema 2
+
+# Tema 2
 # Sample vector
 x <- c("A", "C", "B")
 
 # ifelse with several conditions
-res<- ifelse(x == "C" | x == "A", "Group 1", "Group 2")
+res <- ifelse(x == "C" | x == "A", "Group 1", "Group 2")
 
 # Output
 res
-install.packages(tidyverse)
-library("")
-library(tidyverse)
-library(readxl)
 
+# Instalar paquetes
+install.packages("tidyverse")  
+library(tidyverse)             
+
+install.packages("readxl")     
+library(readxl)                
 
 
 
---
+++ R proyects_Diego_Calderón/proyectos_r/scripts/class_1_reconocimiento_r_17_10_2025.R
@@ -0,0 +1,17 @@
+#######################################################################
+#UNIVERSIDAD TECNOLOGICA UTE
+#DOCENTE: CRISTIAN RIVADENEIRA
+#AUTOR: DIEGO CALDERON
+#FECHA:17/10/2025
+#TEMA: INDRODUCCION AL SOFTWARE R
+#######################################################################
+# Instalar paquetes
+install.packages("tidyverse")
+#importación de libreria
+library(tidyverse)
+path <- "Y:/Estadistica_II/R proyects_Diego_Calderón/t_clase2/in/movies.csv"
+df <- read_csv(path)
+
+
+
+
